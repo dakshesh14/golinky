d0f44c66aa057dfd2df4e85e8f7b1d7f412f6a1f
dakshesh14 <65905942+dakshesh14@users.noreply.github.com>
2025-09-09 23:25:00 +0530

    chore: made config file and main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+
+	"github.com/dakshesh14/golinky/internal/config"
+	"github.com/dakshesh14/golinky/internal/infrastructure/db"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	_, err = db.InitDb(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,49 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppEnv      string
+	AppPort     string
+	DatabaseURL string
+}
+
+func LoadConfig() (*Config, error) {
+	_ = godotenv.Load()
+
+	databaseURL, err := MustGetEnv("DATABASE_URL")
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := &Config{
+		AppEnv:      GetEnvOrDefault("APP_ENVIRONMENT", "local"),
+		AppPort:     GetEnvOrDefault("APP_PORT", "8080"),
+		DatabaseURL: databaseURL,
+	}
+
+	return cfg, nil
+}
+
+func MustGetEnv(key string) (string, error) {
+	value := os.Getenv(key)
+	if value == "" {
+		return "", fmt.Errorf("environment variable %s not set", key)
+	}
+
+	return value, nil
+}
+
+func GetEnvOrDefault(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+
+	return value
+}
