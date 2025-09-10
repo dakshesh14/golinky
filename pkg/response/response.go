afa3de9d06ff5ec110385071fbda3ecf4c9c841c
dakshesh14 <65905942+dakshesh14@users.noreply.github.com>
2025-09-10 20:36:44 +0530

    feat: made response function

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,1 +1,19 @@
 package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteJson(w http.ResponseWriter, status int, v interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(v)
+}
+
+func WriteError(w http.ResponseWriter, status int, msg string, details interface{}) {
+	WriteJson(w, status, map[string]interface{}{
+		"error":   msg,
+		"details": details,
+	})
+}
